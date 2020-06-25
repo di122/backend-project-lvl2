7fc9cb2c8d93fb3bd60debed481396ea057c2a40
di122 <1@122.ru>
2020-06-25 10:17:49 +0300

    update package

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,8 @@
 import program from 'commander';
 
 program
-  .description('Compares two configuration files and shows difference')
-  .option('-f, --format [type]', 'Output format')
   .version('1.0.0')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows difference')
+  .option('-f, --format [type]', 'Output formats: standard, plain, json')
   .parse(process.argv)
